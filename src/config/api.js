bc8952954d9044dc9d8b2a643fffb0fbebcb883a
Yandi Riswandi <yandiriswandi@gmail.com>
2022-04-22 01:09:37 +0700

    version1.1

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 export const API = () => {
   const baseUrl = 
-  process.env.SERVER_URL ||
+  process.env.REACT_APP_SERVER_URL ||
   'https://b32-backend-yandi.herokuapp.com/api/v1'||
   "http://localhost:5000/api/v1";
 
